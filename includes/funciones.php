0f860038341b56e02329863ee9595c512caf2d7a
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-04 20:45:08 -0300

    Añadido a funciones globales pagina_actual, para verificar visualmente que estamos en esa pagina

--- includes/funciones.php
+++ includes/funciones.php
@@ -10,3 +10,12 @@ function s($html) : string {
     $s = htmlspecialchars($html);
     return $s;
 }
+
+function pagina_actual($path) : bool {
+    // 1. Usamos REQUEST_URI que es más confiable
+    // 2. Usamos '??' para evitar errores si la variable no está definida
+    $url_actual = $_SERVER['REQUEST_URI'] ?? '/';
+
+
+    return str_contains($url_actual, $path);
+}
