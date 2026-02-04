d5002c7bcd1f9ae73e62509e08336ed41fe60bf0
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-04 12:51:22 -0300

    Añadido require de alertas

--- views/auth/login.php
+++ views/auth/login.php
@@ -2,9 +2,13 @@
     <h2 class="auth__heading">
         <?php echo $titulo; ?>
     </h2>
-    <p class="auth__texto">Inicia Sesión</p>
+    <p class="auth__texto">Inicia Sesión en DevWebCamp</p>
 
-    <form class="formulario">
+    <?php
+        require_once __DIR__ . '/../templates/alertas.php';
+    ?>
+
+    <form method="POST" action="/login" class="formulario">
         <div class="formulario__campo">
             <label for="email" class="formulario__label">Email</label>
             <input 
