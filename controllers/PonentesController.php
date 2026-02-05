e984d85b21edba0f466c5821a56c8270834cb823
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-05 15:51:17 -0300

    Leer la imagen del Ponente y generar version png y webp

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Intervention\Image\ImageManagerStatic as Image;
 use Model\Ponente;
 use MVC\Router;
 
@@ -18,6 +19,23 @@ class PonentesController {
         $ponente = new Ponente;
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Leer imagen
+            if (!empty($_FILES['imagen']['tmp_name'])) {
+                $carpeta_imagenes = '../public/img/speakers';
+
+                // Crear la carpeta si no existe
+                if (!is_dir($carpeta_imagenes)) {
+                    mkdir($carpeta_imagenes, 0777, true);
+                }
+
+                $image_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('png', 80);
+                $image_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('webp', 80);
+
+                $nombre_imagen= md5(uniqid( rand(), true));
+
+                $_POST['imagen'] = $nombre_imagen;
+            }
+
             $ponente->sincronizar($_POST);
 
             // Validar 
