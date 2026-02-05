b32c96418edecfde1611ccd1a87b30660fb08ea4
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-05 12:32:38 -0300

    Agregado el formulario para ingresar informacion personal y extra

--- views/admin/ponentes/formulario.php
+++ views/admin/ponentes/formulario.php
@@ -3,26 +3,73 @@
 
     <div class="formulario__campo">
         <label for="nombre" class="formulario__label">Nombre</label>
-        <input 
-            type="text" 
-            name="nombre" 
+        <input
+            type="text"
+            name="nombre"
             id="nombre"
             class="formulario__input"
             placeholder="Nombre Ponente"
-            value="<?php echo $ponente->nombre ?? '';?>"
-            > 
+            value="<?php echo $ponente->nombre ?? ''; ?>">
     </div>
 
     <div class="formulario__campo">
         <label for="apellido" class="formulario__label">Apellido</label>
-        <input 
-            type="text" 
-            name="apellido" 
+        <input
+            type="text"
+            name="apellido"
             id="apellido"
             class="formulario__input"
             placeholder="Apellido Ponente"
-            value="<?php echo $ponente->apellido ?? '';?>"
-            > 
+            value="<?php echo $ponente->apellido ?? ''; ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label for="ciudad" class="formulario__label">Ciudad</label>
+        <input
+            type="text"
+            name="ciudad"
+            id="ciudad"
+            class="formulario__input"
+            placeholder="Ciudad Ponente"
+            value="<?php echo $ponente->ciudad ?? ''; ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label for="pais" class="formulario__label">País</label>
+        <input
+            type="text"
+            name="pais"
+            id="pais"
+            class="formulario__input"
+            placeholder="País Ponente"
+            value="<?php echo $ponente->pais ?? ''; ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label for="imagen" class="formulario__label">Imagen</label>
+        <input
+            type="file"
+            name="imagen"
+            id="imagen"
+            class="formulario__input formulario__input--file">
+    </div>
+</fieldset>
+
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">Información Personal Extra</legend>
+
+    <div class="formulario__campo">
+        <label for="tags_input" class="formulario__label">Áreas de Experiencia (separadas por coma)</label>
+        <input
+            type="text"
+            id="tags_input"
+            class="formulario__input"
+            placeholder="Ej. Node.js, PHP, CSS, Laravel, UX / UI"
+        >
+
+        <div id="tags" class="formulario__listado"></div>
+        <input type="hidden" name="tags" value="<?php echo $ponente->tags ?? ''; ?>">
+
     </div>
 
 </fieldset>
