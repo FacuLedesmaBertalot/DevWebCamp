df6c342df96852dbefb3faededb56953cd0858f9
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-09 11:10:09 -0300

    Añadido formulario a eventos

---
+++ views/admin/eventos/formulario.php
@@ -0,0 +1,26 @@
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">Información Evento</legend>
+
+    <div class="formulario__campo">
+        <label for="nombre" class="formulario__label">Nombre Evento</label>
+        <input
+            type="text"
+            name="nombre"
+            id="nombre"
+            class="formulario__input"
+            placeholder="Nombre Evento">
+    </div>
+
+    <div class="formulario__campo">
+        <label for="descripcion" class="formulario__label">Descripción Evento</label>
+        <textarea
+            name="descripcion"
+            id="descripcion"
+            class="formulario__input"
+            placeholder="Descripción Evento"
+            rows="8"
+            >
+        </textarea>
+    </div>
+
+</fieldset>
