317306e072cb7edb670b9f9115bc14c291937427
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-09 13:14:28 -0300

    Añadido dia para seleccionar con radio

--- views/admin/eventos/formulario.php
+++ views/admin/eventos/formulario.php
@@ -23,11 +23,10 @@
 
     <div class="formulario__campo">
         <label for="categoria" class="formulario__label">Categoría o Tipo de Evento</label>
-        <select 
-        class="formulario__select"
-        id="categoria"
-        name="categoria_id"
-        >
+        <select
+            class="formulario__select"
+            id="categoria"
+            name="categoria_id">
             <option value="" selected disabled>-- Seleccionar --</option>
             <?php foreach ($categorias as $categoria) { ?>
                 <option value="<?php echo $categoria->id; ?>">
@@ -39,5 +38,24 @@
 
 
 
+    <div class="formulario__campo">
+        <label for="categoria" class="formulario__label">Selecciona el Día</label>
+
+        <div class="formulario__radio">
+            <?php foreach ($dias as $dia) {  ?>
+                <div>
+                    <label for="<?php echo strtolower($dia->nombre); ?>"> <?php echo $dia->nombre ?></label>
+
+                    <input 
+                    type="radio"
+                    id="<?php echo strtolower($dia->nombre); ?>"
+                    name="dia"
+                    value="<?php echo $dia->id ?>"
+                    >
+                </div>
+            <?php } ?>
+        </div>
+
+    </div>
 
 </fieldset>
