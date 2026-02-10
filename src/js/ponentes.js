ecc6343fe297146f7d5109dc7fba17675331c515
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-10 16:58:58 -0300

    Filtra ponentes por busqueda

--- src/js/ponentes.js
+++ src/js/ponentes.js
@@ -7,6 +7,8 @@
 
         obtenerPonentes();
 
+        ponentesInput.addEventListener('input', buscarPonentes);
+
         async function obtenerPonentes() {
             const url = `/api/ponentes`;
 
@@ -23,6 +25,20 @@
                     id: ponente.id
                 }
             })
+        }
+
+        function buscarPonentes(e) {
+            const busqueda = e.target.value;
+
+            if (busqueda.length > 3) {
+                const expresion = new RegExp(busqueda, "i");
+                ponentesFiltrados = ponentes.filter(ponente => {
+                    if (ponente.nombre.toLowerCase().search(expresion) != -1) {
+                        return ponente;
+                    }
+                })
+            }
+
         }
     }
 
