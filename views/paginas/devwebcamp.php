5d7ee5bbf33838105407dbd48460ba3d4bc2a8b5
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-11 19:38:13 -0300

    Inicio de la vista publica, inicializado paginas/

---
+++ views/paginas/devwebcamp.php
@@ -0,0 +1,24 @@
+<main class="devwebcamp">
+    <h2 class="devwebcamp__heading"><?php echo $titulo; ?></h2>
+    <p class="devwebcamp__descripcion">Conoce la Conferencia Más Importante de Latinoamérica</p>
+
+    <div class="devwebcamp__grid">
+        <div class="devwebcamp__imagen">
+            <picture>
+                <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+                <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+                <img loading="lazy" width="200" height="300" src="build/img/sobre_devwebcamp.jpg" alt="Imagen DevWebCamp">
+            </picture>
+        </div>
+
+        <div class="devwebcamp__contenido">
+            <p class="devwebcamp__texto">
+                DevWebCamp es la conferencia definitiva para los apasionados del desarrollo web, diseñada para unir a la comunidad de creadores digitales de todos los niveles. Aquí encontrarás un espacio dedicado a la innovación, donde expertos de la industria comparten sus conocimientos sobre las tecnologías más punteras, desde frameworks de Frontend y diseño UX/UI, hasta arquitecturas Backend escalables y seguridad informática.
+            </p>
+
+            <p class="devwebcamp__texto">
+                Nuestra misión es fomentar el networking y el aprendizaje continuo en un ambiente inclusivo. A través de talleres prácticos y charlas inspiradoras, DevWebCamp ofrece oportunidades únicas tanto para estudiantes que buscan su primera experiencia, como para desarrolladores senior que desean perfeccionar sus habilidades. Es el lugar ideal para actualizarse, conectar con colegas y descubrir juntos el futuro de la web.
+            </p>
+        </div>
+    </div>
+</main>
