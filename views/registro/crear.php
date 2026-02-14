27c75222cf080573fbc0c1c50832b7390766fa77
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-14 17:12:21 -0300

    Añadida estructura html y js a la vista, con sus estilos

--- views/registro/crear.php
+++ views/registro/crear.php
@@ -1,3 +1,5 @@
+<script src="https://www.paypal.com/sdk/js?client-id=Ac9bbug3KuBkH5iu7FrTRDtxPRoCl4dAaPyQmhLdjOL_zdXxCW66jmGBn3qMzTZw15cCrrkx3MIrZgxv&components=buttons&disable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"></script>
+
 <main class="registro">
     <h2 class="registro__heading"><? echo $titulo; ?></h2>
     <p class="registro__descripcion">Elige tu Plan</p>
@@ -8,14 +10,11 @@
             <ul class="paquete__lista">
                 <li class="paquete__elemento">Acceso Virtual a DevWebCamp</li>
             </ul>
-
             <p class="paquete__precio">$0</p>
-
             <form method="POST" action="/finalizar-registro/gratis">
                 <input class="paquetes__submit" type="submit" value="Inscripción Gratis">
             </form>
         </div>
-
 
         <div class="paquete">
             <h3 class="paquete__nombre">Pase Presencial</h3>
@@ -27,11 +26,14 @@
                 <li class="paquete__elemento">Camisa del Evento</li>
                 <li class="paquete__elemento">Comida y Bebida</li>
             </ul>
-
             <p class="paquete__precio">$199</p>
+
+            <div id="smart-button-container">
+                <div style="text-align: center;">
+                    <div id="paypal-button-container"></div>
+                </div>
+            </div>
         </div>
-
-
 
         <div class="paquete">
             <h3 class="paquete__nombre">Pase Virtual</h3>
@@ -41,8 +43,60 @@
                 <li class="paquete__elemento">Acceso a Talleres y Conferencias</li>
                 <li class="paquete__elemento">Acceso a las Grabaciones</li>
             </ul>
-
             <p class="paquete__precio">$49</p>
         </div>
     </div>
 </main>
+
+
+<script>
+    function initPayPalButton() {
+        paypal.Buttons({
+            style: {
+                shape: 'rect',
+                color: 'blue',
+                layout: 'vertical',
+                label: 'pay',
+            },
+
+            createOrder: function(data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        "description": "1",
+                        "amount": {
+                            "currency_code": "USD",
+                            "value": 199
+                        }
+                    }]
+                });
+            },
+
+            onApprove: function(data, actions) {
+                return actions.order.capture().then(function(orderData) {
+
+                const datos = new FormData();
+                datos.append('paquete_id', orderData.purchase_units[0].description);
+                datos.append('pago_id', orderData.purchase_units[0].payments.captures[0].id);
+
+                fetch('/finalizar-registro/pagar', {
+                    method: 'POST',
+                    body: datos
+                }).
+                then( respuesta => respuesta.json()).
+                then(resultado => {
+                    if (resultado.resultado) {
+                        actions.redirect('http://devwebcamp.test/finalizar-registro/conferencias');
+                    }
+                })
+
+                });
+            },
+
+            onError: function(err) {
+                console.log(err);
+            }
+        }).render('#paypal-button-container');
+    }
+
+    initPayPalButton();
+</script>
