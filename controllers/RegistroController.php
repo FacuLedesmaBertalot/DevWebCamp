67bcb061b26930dd5ddc2aaa4d8c887d0e0cf18f
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-14 17:11:32 -0300

    Almacenando el registro paypal

--- controllers/RegistroController.php
+++ controllers/RegistroController.php
@@ -10,7 +10,7 @@ use MVC\Router;
 
 class RegistroController {
 
-    public static function crear(Router $router) { 
+    public static function crear(Router $router) {
 
         if (!is_auth()) {
             header('Location: /');
@@ -18,32 +18,32 @@ class RegistroController {
 
         // Verificar si el usuario ya esta registrado
         $registro = Registro::where('usuario_id', $_SESSION['id']);
-    
+
         if (isset($registro) && $registro->paquete_id === "3") {
             header('Location: /boleto?id=' . urlencode($registro->token));
-        } 
+        }
 
         $router->render('registro/crear', [
             'titulo' => 'Finalizar Registro'
         ]);
     }
 
-    public static function gratis(Router $router) { 
+    public static function gratis(Router $router) {
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            
+
             if (!is_auth()) {
                 header('Location: /login');
             }
 
-        // Verificar si el usuario ya esta registrado
-        $registro = Registro::where('usuario_id', $_SESSION['id']);
-    
-        if (isset($registro) && $registro->paquete_id === "3") {
-            header('Location: /boleto?id=' . urlencode($registro->token));
-        } 
+            // Verificar si el usuario ya esta registrado
+            $registro = Registro::where('usuario_id', $_SESSION['id']);
 
-            $token =substr( md5(uniqid(rand(), true)), 0, 8 );
+            if (isset($registro) && $registro->paquete_id === "3") {
+                header('Location: /boleto?id=' . urlencode($registro->token));
+            }
+
+            $token = substr(md5(uniqid(rand(), true)), 0, 8);
 
             // Crear registro
             $datos = [
@@ -62,13 +62,12 @@ class RegistroController {
         }
     }
 
-    public static function boleto(Router $router) { 
+    public static function boleto(Router $router) {
         // Validar la URL
         $id = $_GET['id'];
 
         if (!$id || !strlen($id) === 8) {
             header('Location: /');
-
         }
 
         // Buscar en la db
@@ -88,5 +87,36 @@ class RegistroController {
         ]);
     }
 
+
+    public static function pagar(Router $router) {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            if (!is_auth()) {
+                header('Location: /login');
+            }
+
+            // Validar que post no venga vacio
+            if (empty($_POST)) {
+                echo json_encode([]);
+                return;
+            }
+
+            // Crear el Registro
+
+            $datos = $_POST;
+            $datos['token'] = substr(md5(uniqid(rand(), true)), 0, 8);
+            $datos['usuario_id'] = $_SESSION['id'];
+
+            try {
+                $registro = new Registro($datos);
+                $resultado = $registro->guardar();
+                echo json_encode($resultado);
+            } catch (\Throwable $th) {
+                echo json_encode([
+                    'resultado' => 'error'
+                ]);
+            }
+        }
+    }
 }
-?>
