f9b4c65cc72fcc3350ca15949d3582909722ff4f
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-14 13:19:46 -0300

    Añadido enrutador hacia /boleto

--- public/index.php
+++ public/index.php
@@ -68,6 +68,9 @@ $router->get('/admin/regalos', [RegalosController::class, 'index']);
 $router->get('/finalizar-registro', [RegistroController::class, 'crear']);
 $router->post('/finalizar-registro/gratis', [RegistroController::class, 'gratis']);
 
+// Boleto Virtual
+$router->get('/boleto', [RegistroController::class, 'boleto']);
+
 // Área Pública
 $router->get('/', [PaginasController::class, 'index']);
 $router->get('/devwebcamp', [PaginasController::class, 'evento']);
