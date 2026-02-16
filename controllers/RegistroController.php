c0fe3e1dda9f228b6ea16da6b1898f315368ce1e
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-16 15:23:57 -0300

    Añadida al metodo de conferencias la logica para la vista de los regalos

--- controllers/RegistroController.php
+++ controllers/RegistroController.php
@@ -9,6 +9,7 @@ use Model\Evento;
 use Model\Hora;
 use Model\Paquete;
 use Model\Ponente;
+use Model\Regalo;
 use Model\Registro;
 use Model\Usuario;
 use MVC\Router;
@@ -171,9 +172,12 @@ class RegistroController
             }
         }
 
+        $regalos = Regalo::all('ASC');
+
         $router->render('registro/conferencias', [
             'titulo' => 'Elige Workshops y Conferencias',
-            'eventos' => $eventos_formateados
+            'eventos' => $eventos_formateados,
+            'regalos' => $regalos
         ]);
     }
 }
