f83cafced2abdcf3e7ee4f685ccbf653edb75e60
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-16 12:48:18 -0300

    Listando los eventos seleccionados por el usuario con js

--- src/js/registro.js
+++ src/js/registro.js
@@ -1,12 +1,15 @@
+import Swal from "sweetalert2";
+
 (function() {
     let eventos = [];
 
+    const resumen = document.querySelector('#registro-resumen');
     const eventosBoton = document.querySelectorAll('.evento__agregar');
     eventosBoton.forEach(boton => boton.addEventListener('click', seleccionarEvento));
 
     function seleccionarEvento({target}) {
 
-
+        if (eventos.length < 5) {
         // Deshabilitar el evento
         target.disabled = true;
 
@@ -15,7 +18,58 @@
             titulo: target.parentElement.querySelector('.evento__nombre').textContent.trim()
         }];
 
-        console.log(eventos);
+        mostrarEventos();
+        } else {
+            Swal.fire({
+                titulo: 'Error',
+                text: 'Máximo 5 Eventos por Registro',
+                icon: 'error',
+                confirmButtonText: 'OK',
+                confirmButtonColor: '#007df4'
+            })
+        }
+    }
+
+    function mostrarEventos() {
+        // Limpiar el HTML
+        limpiarEventos();
+
+        if (eventos.length > 0) {
+            eventos.forEach( evento => {
+                const eventoDOM = document.createElement('DIV');
+                eventoDOM.classList.add('registro__evento');
+
+                const titulo = document.createElement('H3');
+                titulo.classList.add('registro__nombre');
+                titulo.textContent = evento.titulo;
+
+                const botonEliminar = document.createElement('BUTTON');
+                botonEliminar.classList.add('registro__eliminar');
+                botonEliminar.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+                botonEliminar.onclick = function() {
+                    eliminarEvento(evento.id);
+                }
+
+                // Renderizar en el HTML
+                eventoDOM.appendChild(titulo);
+                eventoDOM.appendChild(botonEliminar);
+                resumen.appendChild(eventoDOM);
+
+            });
+        }
+    }
+    
+    function eliminarEvento(id) {
+        eventos = eventos.filter(evento => evento.id !== id);
+        const botonAgregar = document.querySelector(`[data-id="${id}"]`);
+        botonAgregar.disabled = false;
+        mostrarEventos();
+    }
+
+    function limpiarEventos() {
+        while (resumen.firstChild) {
+            resumen.removeChild(resumen.firstChild);
+        }
     }
 
 })();
