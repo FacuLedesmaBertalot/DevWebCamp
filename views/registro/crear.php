150b5a24c882354f75f36656268253a9f05d5d47
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-16 18:42:32 -0300

    Creando el boton paypal para el pago virtual

--- views/registro/crear.php
+++ views/registro/crear.php
@@ -1,8 +1,6 @@
-<script src="https://www.paypal.com/sdk/js?client-id=Ac9bbug3KuBkH5iu7FrTRDtxPRoCl4dAaPyQmhLdjOL_zdXxCW66jmGBn3qMzTZw15cCrrkx3MIrZgxv&components=buttons&disable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"></script>
-
-<main class="registro">
-    <h2 class="registro__heading"><? echo $titulo; ?></h2>
-    <p class="registro__descripcion">Elige tu Plan</p>
+<main class="pagina">
+    <h2 class="pagina__heading"><?php echo $titulo; ?></h2>
+    <p class="pagina__descripcion">Elige tu Plan</p>
 
     <div class="paquetes__grid">
         <div class="paquete">
@@ -30,7 +28,7 @@
 
             <div id="smart-button-container">
                 <div style="text-align: center;">
-                    <div id="paypal-button-container"></div>
+                    <div id="paypal-button-container-presencial"></div>
                 </div>
             </div>
         </div>
@@ -44,13 +42,23 @@
                 <li class="paquete__elemento">Acceso a las Grabaciones</li>
             </ul>
             <p class="paquete__precio">$49</p>
+
+            <div id="smart-button-container">
+                <div style="text-align: center;">
+                    <div id="paypal-button-container-virtual"></div>
+                </div>
+            </div>
         </div>
     </div>
 </main>
 
+<script src="https://www.paypal.com/sdk/js?client-id=Ac9bbug3KuBkH5iu7FrTRDtxPRoCl4dAaPyQmhLdjOL_zdXxCW66jmGBn3qMzTZw15cCrrkx3MIrZgxv&components=buttons&disable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"></script>
 
 <script>
     function initPayPalButton() {
+        // ------------------------------------------------
+        // BOTÓN 1: PASE PRESENCIAL
+        // ------------------------------------------------
         paypal.Buttons({
             style: {
                 shape: 'rect',
@@ -62,10 +70,10 @@
             createOrder: function(data, actions) {
                 return actions.order.create({
                     purchase_units: [{
-                        "description": "1",
+                        "description": "1", // 1 = Pase Presencial
                         "amount": {
                             "currency_code": "USD",
-                            "value": 199
+                            "value": 199 // Precio del presencial
                         }
                     }]
                 });
@@ -74,28 +82,84 @@
             onApprove: function(data, actions) {
                 return actions.order.capture().then(function(orderData) {
 
-                const datos = new FormData();
-                datos.append('paquete_id', orderData.purchase_units[0].description);
-                datos.append('pago_id', orderData.purchase_units[0].payments.captures[0].id);
+                    const datos = new FormData();
+                    datos.append('paquete_id', orderData.purchase_units[0].description);
+                    datos.append('pago_id', orderData.purchase_units[0].payments.captures[0].id);
 
-                fetch('/finalizar-registro/pagar', {
-                    method: 'POST',
-                    body: datos
-                }).
-                then( respuesta => respuesta.json()).
-                then(resultado => {
-                    if (resultado.resultado) {
-                        actions.redirect('http://devwebcamp.test/finalizar-registro/conferencias');
-                    }
-                })
-
+                    fetch('/finalizar-registro/pagar', {
+                        method: 'POST',
+                        body: datos
+                    })
+                    .then(respuesta => respuesta.json())
+                    .then(resultado => {
+                        if (resultado.resultado === true) {
+                            window.location.href = '/finalizar-registro/conferencias';
+                        } else {
+                            // En producción puedes cambiar esto por un SweetAlert
+                            alert('Hubo un error: ' + resultado.error); 
+                        }
+                    })
+                    .catch(error => console.log(error));
                 });
             },
 
             onError: function(err) {
                 console.log(err);
             }
-        }).render('#paypal-button-container');
+        }).render('#paypal-button-container-presencial'); // ID del Div Presencial
+
+
+        // ------------------------------------------------
+        // BOTÓN 2: PASE VIRTUAL
+        // ------------------------------------------------
+        paypal.Buttons({
+            style: {
+                shape: 'rect',
+                color: 'blue',
+                layout: 'vertical',
+                label: 'pay',
+            },
+
+            createOrder: function(data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        "description": "2", // 2 = Pase Virtual
+                        "amount": {
+                            "currency_code": "USD",
+                            "value": 49 // Precio del virtual
+                        }
+                    }]
+                });
+            },
+
+            onApprove: function(data, actions) {
+                return actions.order.capture().then(function(orderData) {
+
+                    const datos = new FormData();
+                    datos.append('paquete_id', orderData.purchase_units[0].description);
+                    datos.append('pago_id', orderData.purchase_units[0].payments.captures[0].id);
+
+                    fetch('/finalizar-registro/pagar', {
+                        method: 'POST',
+                        body: datos
+                    })
+                    .then(respuesta => respuesta.json())
+                    .then(resultado => {
+                        if (resultado.resultado === true) {
+                            window.location.href = '/boleto?id=' + resultado.token;
+                        } else {
+                            // En producción puedes cambiar esto por un SweetAlert
+                            alert('Hubo un error: ' + resultado.error); 
+                        }
+                    })
+                    .catch(error => console.log(error));
+                });
+            },
+
+            onError: function(err) {
+                console.log(err);
+            }
+        }).render('#paypal-button-container-virtual'); // ID del Div Virtual
     }
 
     initPayPalButton();
