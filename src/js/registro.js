159ba3ac6fd5534348e26a7bb481fab030fcea0c
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-16 15:35:29 -0300

    Validando el registro y el regalo

--- src/js/registro.js
+++ src/js/registro.js
@@ -1,75 +1,106 @@
 import Swal from "sweetalert2";
 
-(function() {
-    let eventos = [];
+(function () {
+  let eventos = [];
 
-    const resumen = document.querySelector('#registro-resumen');
-    const eventosBoton = document.querySelectorAll('.evento__agregar');
-    eventosBoton.forEach(boton => boton.addEventListener('click', seleccionarEvento));
+  const resumen = document.querySelector("#registro-resumen");
 
-    function seleccionarEvento({target}) {
+  if (resumen) {
+    const eventosBoton = document.querySelectorAll(".evento__agregar");
+    eventosBoton.forEach((boton) =>
+      boton.addEventListener("click", seleccionarEvento),
+    );
 
-        if (eventos.length < 5) {
+    const formularioRegistro = document.querySelector("#registro");
+    formularioRegistro.addEventListener("submit", submitFormulario);
+
+    function seleccionarEvento({ target }) {
+      if (eventos.length < 5) {
         // Deshabilitar el evento
         target.disabled = true;
 
-        eventos = [...eventos, {
+        eventos = [
+          ...eventos,
+          {
             id: target.dataset.id,
-            titulo: target.parentElement.querySelector('.evento__nombre').textContent.trim()
-        }];
+            titulo: target.parentElement
+              .querySelector(".evento__nombre")
+              .textContent.trim(),
+          },
+        ];
 
         mostrarEventos();
-        } else {
-            Swal.fire({
-                titulo: 'Error',
-                text: 'Máximo 5 Eventos por Registro',
-                icon: 'error',
-                confirmButtonText: 'OK',
-                confirmButtonColor: '#007df4'
-            })
-        }
+      } else {
+        Swal.fire({
+          title: "Error",
+          text: "Máximo 5 Eventos por Registro",
+          icon: "error",
+          confirmButtonText: "OK",
+          confirmButtonColor: "#007df4",
+        });
+      }
     }
 
     function mostrarEventos() {
-        // Limpiar el HTML
-        limpiarEventos();
+      // Limpiar el HTML
+      limpiarEventos();
 
-        if (eventos.length > 0) {
-            eventos.forEach( evento => {
-                const eventoDOM = document.createElement('DIV');
-                eventoDOM.classList.add('registro__evento');
+      if (eventos.length > 0) {
+        eventos.forEach((evento) => {
+          const eventoDOM = document.createElement("DIV");
+          eventoDOM.classList.add("registro__evento");
 
-                const titulo = document.createElement('H3');
-                titulo.classList.add('registro__nombre');
-                titulo.textContent = evento.titulo;
+          const titulo = document.createElement("H3");
+          titulo.classList.add("registro__nombre");
+          titulo.textContent = evento.titulo;
 
-                const botonEliminar = document.createElement('BUTTON');
-                botonEliminar.classList.add('registro__eliminar');
-                botonEliminar.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-                botonEliminar.onclick = function() {
-                    eliminarEvento(evento.id);
-                }
+          const botonEliminar = document.createElement("BUTTON");
+          botonEliminar.classList.add("registro__eliminar");
+          botonEliminar.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+          botonEliminar.onclick = function () {
+            eliminarEvento(evento.id);
+          };
 
-                // Renderizar en el HTML
-                eventoDOM.appendChild(titulo);
-                eventoDOM.appendChild(botonEliminar);
-                resumen.appendChild(eventoDOM);
-
-            });
-        }
+          // Renderizar en el HTML
+          eventoDOM.appendChild(titulo);
+          eventoDOM.appendChild(botonEliminar);
+          resumen.appendChild(eventoDOM);
+        });
+      }
     }
-    
+
     function eliminarEvento(id) {
-        eventos = eventos.filter(evento => evento.id !== id);
-        const botonAgregar = document.querySelector(`[data-id="${id}"]`);
-        botonAgregar.disabled = false;
-        mostrarEventos();
+      eventos = eventos.filter((evento) => evento.id !== id);
+      const botonAgregar = document.querySelector(`[data-id="${id}"]`);
+      botonAgregar.disabled = false;
+      mostrarEventos();
     }
 
     function limpiarEventos() {
-        while (resumen.firstChild) {
-            resumen.removeChild(resumen.firstChild);
-        }
+      while (resumen.firstChild) {
+        resumen.removeChild(resumen.firstChild);
+      }
     }
 
+    function submitFormulario(e) {
+      e.preventDefault();
+
+      // Obtener regalo
+      const regaloId = document.querySelector("#regalo").value;
+
+      const eventosId = eventos.map((evento) => evento.id);
+
+      if (eventosId.length === 0 || regaloId === "") {
+        Swal.fire({
+          title: "Error",
+          text: "Elige al Menos un Evento y un Regalo",
+          icon: "error",
+          confirmButtonText: "OK",
+          confirmButtonColor: "#007df4",
+        });
+        return;
+      }
+      console.log('registrando')
+    }
+  }
 })();
