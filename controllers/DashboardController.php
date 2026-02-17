4467af6a3ca58444f11f95a2f9ddc00e523a802c
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-17 16:15:33 -0300

    Añadida la logica para calcular los usuarios registrados, tanto en presencial como virtual

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Dotenv\Util\Regex;
 use Model\Registro;
 use Model\Usuario;
 use MVC\Router;
@@ -13,15 +14,21 @@ class DashboardController {
         // Obtener últimos registros
         $registros = Registro::get(5);
 
+        // Calcular los ingresos
+        $virtuales = Registro::total('paquete_id', 2);
+        $presenciales = Registro::total('paquete_id', 1);
+
+        $ingresos = ($virtuales * 46.41) + ($presenciales * 189.54);
+
         foreach ($registros as $registro) {
             $registro->usuario = Usuario::find($registro->usuario_id);
 
         }
 
-
         $router->render('admin/dashboard/index',[
             'titulo' => 'Panel de Administración',
-            'registros' => $registros
+            'registros' => $registros,
+            'ingresos' => $ingresos
         ]);
     }
 }
