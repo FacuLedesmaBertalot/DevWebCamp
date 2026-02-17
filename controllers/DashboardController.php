5d59a90357bcb8539e29a07a1002a9efb7585a9c
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-17 16:06:30 -0300

    Añadido el metodo index para la logica

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,13 +2,26 @@
 
 namespace Controllers;
 
+use Model\Registro;
+use Model\Usuario;
 use MVC\Router;
 
 class DashboardController {
 
     public static function index(Router $router) {
+
+        // Obtener últimos registros
+        $registros = Registro::get(5);
+
+        foreach ($registros as $registro) {
+            $registro->usuario = Usuario::find($registro->usuario_id);
+
+        }
+
+
         $router->render('admin/dashboard/index',[
-            'titulo' => 'Panel de Administración'
+            'titulo' => 'Panel de Administración',
+            'registros' => $registros
         ]);
     }
 }
