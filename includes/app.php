2fd0ff7da367028297209a75f59631d46460a4fa
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-09 13:24:03 -0300

    Commit

--- includes/app.php
+++ includes/app.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Añadir Dotenv
 $dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->safeLoad();
+$dotenv->load();
 
 require 'funciones.php';
 require 'database.php';
